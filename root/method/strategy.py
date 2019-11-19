2b4365e7820cb538396d564a38a9b9a40d79ffbe
Thais Amorim <thasmarinho@gmail.com>
2019-11-19 13:54:23 -0300

    [Bugfix] typo at Strategy.py

--- root/method/strategy.py
+++ root/method/strategy.py
@@ -5,7 +5,7 @@ class Strategy():
     @staticmethod
     def compress(method, filename):
         method = method.strip().lower()
-        print("Compressing ",args.filename[0]," using method",args.method)
+        print("Compressing ",filename," using method",method)
 
         if "huffman" == method:
             compressor = Huffman(filename)
@@ -16,7 +16,7 @@ class Strategy():
     @staticmethod
     def decompress(method, filename):
         method = method.strip().lower()
-        print("Decompressing ",args.filename[0]," using method",args.method)
+        print("Decompressing ",filename," using method",method)
 
         if "huffman" == method:
             compressor = Huffman(filename)

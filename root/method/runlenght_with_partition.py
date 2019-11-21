1fdeb55c76d7cc400a09784ff44a6bb07933835c
simao <lucas64_64@hotmail.com>
2019-11-20 21:08:41 -0300

    Implemented another version of runlenght, but its not working very well

--- root/method/runlenght_with_partition.py
+++ root/method/runlenght_with_partition.py
@@ -1,9 +1,9 @@
-class RunLength():
+class RunLengthPartition():
 
     def __init__(self, filename):
         self.filename = filename
-        self.compress_filename = filename + '.rle'
-        self.decompress_filename = "images/output_huffman.bmp"
+        self.compress_filename = filename + '.rle2'
+        self.decompress_filename = "images/output_rle_partition.bmp"
 
     def analiseImage(self, tolerance):
         all_bytes = self.read_bytes(self.filename)
@@ -29,12 +29,15 @@ class RunLength():
         return False
 
     def compress(self):
+
+        divider = self.decide_divider()
+
         all_bytes = self.read_bytes(self.filename)
         self.initial_bytes_amount = len(all_bytes)
         previous_byte = -1
         count = 0
         output_bits = b''
-        for i, b in enumerate(all_bytes, start=1):
+        for i, b in enumerate(all_bytes[:int(len(all_bytes)/divider)], start=1):
 
             if (b != previous_byte) or count >= 255:
                 if previous_byte != -1:
@@ -47,7 +50,9 @@ class RunLength():
         output_bits = output_bits + bytes([count])
         output_bits = output_bits + bytes([previous_byte])
 
-        self.write_bytes(self.compress_filename, output_bits)
+        output_bits = output_bits + all_bytes[int(len(all_bytes)/divider):]
+        header_bits = bytes([divider])
+        self.write_bytes(self.compress_filename,header_bits, output_bits)
         self.final_bytes_amount = len(output_bits)
         return self.compress_filename, self.initial_bytes_amount, self.final_bytes_amount
 
@@ -57,20 +62,24 @@ class RunLength():
             all_bytes = binaryfile.read()
         return all_bytes
 
-    def write_bytes(self, output_path, output_bytes):
+    def write_bytes(self, output_path, header_bytes, output_bytes):
         with open(output_path, 'wb') as out_file:
+            out_file.write(header_bytes)
             out_file.write(output_bytes)
 
     def decompress(self):
 
         with open(self.filename, 'rb') as input_file:
+            divider = int.from_bytes(input_file.read(1),sys.byteorder)
             rle = input_file.read()
         output_bytes = b''
-        for i in range(0, len(rle), 2):
+        for i in range(0, int(len(rle)/divider), 2):
             rep = int(rle[i])
             value = rle[i + 1]
             for j in range(rep):
                 output_bytes = output_bytes + bytes([value])
+
+        output_bytes = output_bytes + rle[int(len(rle)/divider) : ]
 
         with open(self.decompress_filename, 'wb') as output_file:
             output_file.write(output_bytes)
@@ -109,14 +118,23 @@ class RunLength():
         return 1,0
 
     def decide_divider(self):
+        all_bytes = self.read_bytes(self.filename)
         best_index, best_rep = 1,0
         for i in range(50,255,30):
             index, total_rep = self.analise_repetitions(i)
             if(total_rep/index > best_rep/best_index):
                 best_index,best_rep = index,total_rep
-        print(best_index,best_rep)
+        # print(best_index,best_rep)
+        if((best_rep,best_index > 0.5) and (len(all_bytes)/best_index) <255):
+            # print(len(all_bytes)/best_index)
+            return int(len(all_bytes)/best_index)
+        else:
+            return 1
 
-f = RunLength("images/benchmark.bmp.pdi")
+f = RunLengthPartition("images/benchmark.bmp.rle2")
 # f.analiserRepetitions(100)
-f.decide_divider()
-f.analiseImage(0)
+# x = f.decide_divider()
+# print(x)
+# f.compress()
+# f.analiseImage(0)
+f.decompress()
